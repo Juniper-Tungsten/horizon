c3fe0b6c04c0955352eca5f50f606175879e2e8d
Rahul Sharma <rahuls@juniper.net>
2014-03-13 11:03:56 +0530

    Don't release Floating IP from Instances page and only disassociate it

--- openstack_dashboard/dashboards/project/instances/tables.py
+++ openstack_dashboard/dashboards/project/instances/tables.py
@@ -419,7 +419,7 @@ class SimpleDisassociateIP(tables.Action):
                 fip = fips.pop()
                 api.network.floating_ip_disassociate(request,
                                                      fip.id, target_id)
-                api.network.tenant_floating_ip_release(request, fip.id)
+                #api.network.tenant_floating_ip_release(request, fip.id)
                 messages.success(request,
                                  _("Successfully disassociated "
                                    "floating IP: %s") % fip.ip)
